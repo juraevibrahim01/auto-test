c0f7571cdff0d2ae33bff263fc26fd6ee4238d5e
ibrahimjuraev <juraevibrahim01@mail.ru>
2026-04-22 15:29:09 +0500

    исправили ошибку

--- conftest.py
+++ conftest.py
@@ -1,9 +1,11 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
+    service = Service(executable_path="chromedriver/chromedriver.exe")
+    driver = webdriver.Chrome(service=service)
     driver.get("https://humo.tj/en")
     yield driver
     driver.quit()
